579a1a70952e7e7bfd54fb796241e8ea5f4042c5
Justice <popystone@gmail.cm>
2019-05-03 12:03:00 +0100

    Disable the toolbar title from the forecast fragment

--- app/src/main/java/com/pixelart/geocodingweatherapp/forecastscreen/ForecastFragment.kt
+++ app/src/main/java/com/pixelart/geocodingweatherapp/forecastscreen/ForecastFragment.kt
@@ -8,16 +8,22 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.pixelart.geocodingweatherapp.R
+import kotlinx.android.synthetic.main.fragment_forecast.*
+import androidx.appcompat.app.AppCompatActivity
 
+@Suppress("CAST_NEVER_SUCCEEDS")
 class ForecastFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forecast, container, false)
     }
-
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar!!.title = ""
+        main_collapsing.isTitleEnabled = true
+        main_collapsing.title = "Forecast"
+    }
 
 }
