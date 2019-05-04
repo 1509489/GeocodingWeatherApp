adcf4c850ff7a4b8754a7e7ad1a8cf50f3077a26
Justice <popystone@gmail.cm>
2019-05-04 15:35:55 +0100

    Added implementation to get the four day forecast for each location

--- app/src/main/java/com/pixelart/geocodingweatherapp/ui/forecastscreen/ForecastFragment.kt
+++ app/src/main/java/com/pixelart/geocodingweatherapp/ui/forecastscreen/ForecastFragment.kt
@@ -2,20 +2,24 @@ package com.pixelart.geocodingweatherapp.ui.forecastscreen
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.pixelart.geocodingweatherapp.R
-import kotlinx.android.synthetic.main.fragment_forecast.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.pixelart.geocodingweatherapp.AppController
+import com.pixelart.geocodingweatherapp.common.GlideApp
 import com.pixelart.geocodingweatherapp.common.RxBus
+import com.pixelart.geocodingweatherapp.common.WEATHER_ICONURL
 import com.pixelart.geocodingweatherapp.data.entities.LocationEntity
+import com.pixelart.geocodingweatherapp.data.model.Forecast
 import com.pixelart.geocodingweatherapp.di.fragment.FragmentModule
 import io.reactivex.functions.Consumer
+import kotlinx.android.synthetic.main.fragment_forecast.view.*
 import javax.inject.Inject
 
 @Suppress("CAST_NEVER_SUCCEEDS")
@@ -23,8 +27,10 @@ class ForecastFragment : Fragment() {
 
     @Inject lateinit var viewModel: WeatherViewModel
 
+    private lateinit var rootView: View
     private lateinit var latLon: Pair<Double, Double>
     private var toolbarTitle = ""
+    private lateinit var forecast: ArrayList<Forecast>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,23 +46,67 @@ class ForecastFragment : Fragment() {
                 toolbarTitle = it.locationName
             }
         })
+
+        forecast = ArrayList()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forecast, container, false)
+        rootView = inflater.inflate(R.layout.fragment_forecast, container, false)
+        return rootView
     }
     
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar!!.title = ""
-        main_collapsing.isTitleEnabled = true
-        main_collapsing.title = toolbarTitle
+        rootView.collapsing.isTitleEnabled = true
+        rootView.collapsing.title = toolbarTitle
     }
 
     override fun onResume() {
         super.onResume()
-        viewModel.getForecast(latLon.first, latLon.second).observe(this, Observer {
+        viewModel.getForecast(latLon.first, latLon.second).observe(this, Observer {weatherResponse ->
+
+            for (i in 0 until weatherResponse.list.size step 8 )
+            {
+                forecast.add(Forecast(weatherResponse.list[i].dt, weatherResponse.list[i].weather[0].icon,
+                    weatherResponse.list[i].main.temp))
+
+                Log.d("Timestamp", weatherResponse.list[i].dt.toString())
+            }
+            Log.d("Forecast", forecast.size.toString())
+
+            rootView.apply {
+                tvDayOne.text = forecast[0].timestamp.toString()
+                tvDayTwo.text = forecast[1].timestamp.toString()
+                tvDayThree.text = forecast[2].timestamp.toString()
+                tvDayFour.text = forecast[3].timestamp.toString()
+
+                tvDayOneTemperature.text = forecast[0].temperature.toString()
+                tvDayTwoTemperature.text = forecast[1].temperature.toString()
+                tvDayThreeTemperature.text = forecast[2].temperature.toString()
+                tvDayFourTemperature.text = forecast[3].temperature.toString()
+
+                GlideApp.with(this)
+                    .load("$WEATHER_ICONURL${forecast[0].icon}.png")
+                    .override(100, 100)
+                    .into(ivDayOneIcon)
+
+                GlideApp.with(this)
+                    .load("$WEATHER_ICONURL${forecast[1].icon}.png")
+                    .override(60, 60)
+                    .into(ivDayTwoIcon)
+
+                GlideApp.with(this)
+                    .load("$WEATHER_ICONURL${forecast[2].icon}.png")
+                    .override(60, 60)
+                    .into(ivDayThreeIcon)
+
+                GlideApp.with(this)
+                    .load("$WEATHER_ICONURL${forecast[3].icon}.png")
+                    .override(60, 60)
+                    .into(ivDayFourIcon)
+            }
 
         })
     }
