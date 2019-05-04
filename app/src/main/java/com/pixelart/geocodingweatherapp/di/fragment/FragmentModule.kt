d3a251a55022d0817523c0f6ab9ddb5aa8523a7e
Justice <popystone@gmail.cm>
2019-05-04 12:00:43 +0100

    Added dependency injection implementation for injecting the weather view model

--- app/src/main/java/com/pixelart/geocodingweatherapp/di/fragment/FragmentModule.kt
+++ app/src/main/java/com/pixelart/geocodingweatherapp/di/fragment/FragmentModule.kt
@@ -3,6 +3,8 @@ package com.pixelart.geocodingweatherapp.di.fragment
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.pixelart.geocodingweatherapp.factories.LocationViewModelFactory
+import com.pixelart.geocodingweatherapp.factories.WeatherViewModelFactory
+import com.pixelart.geocodingweatherapp.ui.forecastscreen.WeatherViewModel
 import com.pixelart.geocodingweatherapp.ui.homescreen.LocationViewModel
 import dagger.Module
 import dagger.Provides
@@ -12,6 +14,11 @@ class FragmentModule(private val fragment: Fragment) {
     
     @Provides
     @FragmentScope
-    fun providesLocationViewModel(viewModelFactory: LocationViewModelFactory) =
+    fun providesLocationViewModelGeocoding(viewModelFactory: LocationViewModelFactory) =
         ViewModelProviders.of(fragment, viewModelFactory).get(LocationViewModel::class.java)
+
+    @Provides
+    @FragmentScope
+    fun providesLocationViewModelWeather(viewModelFactory: WeatherViewModelFactory) =
+        ViewModelProviders.of(fragment, viewModelFactory).get(WeatherViewModel::class.java)
 }
