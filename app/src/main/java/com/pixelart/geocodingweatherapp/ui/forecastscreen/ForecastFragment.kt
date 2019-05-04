bf7c42a8c67ba19c35a894c6e1c6477d79c4aecf
Justice <popystone@gmail.cm>
2019-05-04 12:12:57 +0100

    implemented the weather view model in the forecast fragment to get the weather forecast

--- app/src/main/java/com/pixelart/geocodingweatherapp/ui/forecastscreen/ForecastFragment.kt
+++ app/src/main/java/com/pixelart/geocodingweatherapp/ui/forecastscreen/ForecastFragment.kt
@@ -10,9 +10,24 @@ import android.view.ViewGroup
 import com.pixelart.geocodingweatherapp.R
 import kotlinx.android.synthetic.main.fragment_forecast.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.pixelart.geocodingweatherapp.AppController
+import com.pixelart.geocodingweatherapp.di.fragment.FragmentModule
+import javax.inject.Inject
 
 @Suppress("CAST_NEVER_SUCCEEDS")
 class ForecastFragment : Fragment() {
+
+    @Inject lateinit var viewModel: WeatherViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val fragmentComponent = (activity?.application as AppController)
+            .applicationComponent
+            .newFragmentComponent(FragmentModule(this))
+        fragmentComponent.injectForecastScreen(this)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -24,6 +39,13 @@ class ForecastFragment : Fragment() {
         (activity as AppCompatActivity).supportActionBar!!.title = ""
         main_collapsing.isTitleEnabled = true
         main_collapsing.title = "Forecast"
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getForecast(51.5073509, -0.1277583).observe(this, Observer {
+
+        })
     }
 
 }
